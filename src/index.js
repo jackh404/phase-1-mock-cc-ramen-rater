1837ffe4c6692ac3c25ee0f13f9081cdd909f5b3
Jack Henza <jackhenza@gmail.com>
2023-10-11 12:39:43 -0400

    show menu & detail view

--- src/index.js
+++ src/index.js
@@ -1,59 +1,36 @@
-// write your code here
-const ramenMenu = document.getElementById("ramen-menu")
-const form = document.getElementById('new-ramen')
+const menu = document.getElementById("ramen-menu")
+const form = document.getElementById("new-ramen")
 
-const displayHeader = () => {
+const renderRamen = (ramen) => {
+    const image = document.createElement("img")
+    image.setAttribute("src",ramen.image)
+    image.setAttribute("alt",`image of ${ramen.name}`)
+    image.addEventListener("click", ()=> showDetail(ramen))
+
+    menu.appendChild(image)
+}
+
+const fetchRamen = () => {
     fetch("http://localhost:3000/ramens")
     .then(resp => resp.json())
     .then(data => {
-        data.forEach(ramen => {
+        for(ramen of data){
             renderRamen(ramen)
-        })
+        }
+        showDetail(data[0])
     })
 }
 
-const renderRamen = ramen => {
-    const ramenImg = document.createElement("img")
-    const ramenDiv = document.createElement("div")
-
-    ramenImg.src = ramen.image
-    ramenImg.alt = "photo of ramen"
-    ramenImg.addEventListener("click",()=>{
-        const detailImg = document.querySelector('.detail-image')
-        detailImg.src = ramen.image
-
-        const detailName = document.querySelector('.name')
-        detailName.textContent = ramen.name
-
-        const restaurant = document.querySelector('.restaurant')
-        restaurant.textContent = ramen.restaurant
-
-        const rating = document.getElementById('rating-display')
-        rating.textContent = ramen.rating
-
-        const comment = document.getElementById('comment-display')
-        comment.textContent = ramen.comment
-    })
-
-    ramenDiv.appendChild(ramenImg)
-    ramenMenu.appendChild(ramenDiv)
+const showDetail = (ramen) => {
+    const detailImage = document.querySelector(".detail-image")
+    detailImage.setAttribute("src",ramen.image)
+    detailImage.setAttribute("alt",`Image of ${ramen.name}`)
+    
+    document.querySelector(".name").textContent = ramen.name
+    document.querySelector(".restaurant").textContent = ramen.restaurant
+    document.getElementById("rating-display").textContent = ramen.rating
+    document.getElementById("comment-display").textContent = ramen.comment
 }
 
-const createRamen = () => {
-    const newRamen = {
-        "id": 0,
-        "name": form.name.value,
-        "restaurant": form.restaurant.value,
-        "image": form.image.value,
-        "rating": form.rating.value,
-        "comment": form['new-comment'].value
-      }
-    renderRamen(newRamen)
-}
 
-form.addEventListener('submit',e=>{
-    e.preventDefault()
-    createRamen()
-    form.reset()
-})
-displayHeader()
+fetchRamen()
